e70273f36ef303e7a2f37f398accb875694a5ae0
DestinHan <seung4530@naver.com>
2024-09-16 15:06:59 -0400

    updated

--- server.js
+++ server.js
@@ -1,4 +1,9 @@
+// get the express module
 const express = require('express');
+
+const path = require('path');
+
+// instantiates the module
 const app = express();
 const PORT = process.env.PORT || 3001;
 
@@ -6,6 +11,5 @@ app.get('/', (req, res) => {
     res.send('Seung Hoon Han - 108302233');
 });
 
-app.listen(PORT, () => {
-    console.log(`Server is running on port ${PORT}`);
-});
+// start the server
+app.listen(HTTP_PORT, () => console.log(`server listening on http://localhost:${HTTP_PORT}`))
