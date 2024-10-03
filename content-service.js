7354e0d9e942b4b60ec91384094fb907f618e7bd
DestinHan <seung4530@naver.com>
2024-10-03 18:02:16 -0400

    AS2 P2S123

--- content-service.js
+++ content-service.js
@@ -1,1 +1,44 @@
+const fs = require('fs');
 
+let articles = [];
+let categories = [];
+
+// Initialize by loading articles and categories from JSON files
+function initialize() {
+  return new Promise((resolve, reject) => {
+    fs.readFile('./data/articles.json', 'utf8', (err, data) => {
+      if (err) {
+        reject("Unable to load articles.json");
+        return;
+      }
+      articles = JSON.parse(data);
+
+      fs.readFile('./data/categories.json', 'utf8', (err, data) => {
+        if (err) {
+          reject("Unable to load categories.json");
+          return;
+        }
+        categories = JSON.parse(data);
+        resolve();
+      });
+    });
+  });
+}
+
+// Get only published articles
+function getPublishedArticles() {
+  return articles.filter(article => article.published);
+}
+
+// Get all categories
+function getCategories() {
+  return categories;
+}
+
+// Export functions
+module.exports = {
+  initialize,
+  getPublishedArticles,
+  getCategories,
+  getAllArticles: () => articles // Inline function for all articles
+};
--- server.js
+++ server.js
@@ -19,5 +19,14 @@ app.get('/about', (req, res) => {
     res.sendFile(path.join(__dirname, 'views', 'about.html'));
 });
 
+app.get('/articles', (req, res) => {
+    res.json(contentService.getAllArticles());
+  });
+  
+app.get('/categories', (req, res) => {
+    res.json(contentService.getAllCategories());
+  });
+  
+
 // start the server
 app.listen(HTTP_PORT, () => console.log(`server listening on http://localhost:${HTTP_PORT}`))
